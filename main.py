89a7e2b554d854dc4d0eb19e04bf0ce97c820415
Otavio Campos <otavio.campos@ufu.br>
2024-09-03 17:47:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'Referer': 'https://nitter.lucabased.xyz/',
-    'Cookie': 'res=FD41AA63A8AC2B8F8011FB17B9DB22E7723DD34E12971'
 }
 
 # Inicializa o cliente do Bluesky
